16b6dbee8d6b8598a1568a138e89e912f519c15f
valmargutt <valmargutt@gmail.com>
2023-11-28 15:15:54 +0000

    Lists are here
    Lists look pretty, no functionality

---
+++ Toodler/src/components/ListName/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+	text: {
+		margin: 10,
+		textAlign: "center",
+	},
+	button: {
+		paddingTop: 10,
+		paddingBottom: 10,
+		marginBottom: 10,
+		marginTop: 10,
+		marginLeft: 20,
+		marginRight: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderWidth: 3,
+		borderColor: "black",
+		borderRadius: 5,
+	},
+});
