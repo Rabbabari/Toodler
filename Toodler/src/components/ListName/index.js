c70ba642b58837fbe97e8a1d9b2d10378dcba73c
valmargutt <valmargutt@gmail.com>
2023-11-29 12:33:29 +0000

    Lists functionality
    some more functionality have been implemented for lists

--- Toodler/src/components/ListName/index.js
+++ Toodler/src/components/ListName/index.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { Text, View, TouchableHighlight } from "react-native";
+import PropTypes from "prop-types";
+import { Text, View, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import styles from "./styles";
 
-const ListName = ({ id, name, color }) => {
-	console.log(id, name, color);
+const ListName = ({ id, name, color, onLongPress, isSelected }) => {
 	return (
-		<View>
-			<TouchableHighlight
-				style={[styles.button, { backgroundColor: color }]}
-			>
+		<TouchableOpacity
+			style={[styles.button, { backgroundColor: color }]}
+			onLongPress={() => onLongPress(id)}
+		>
+			{isSelected ? (
+				<AntDesign name="checkcircle" style={styles.checkmark} />
+			) : (
+				<></>
+			)}
+			<View style={{ opacity: isSelected ? 0.5 : 1 }}>
 				<Text style={styles.text}>{name}</Text>
-			</TouchableHighlight>
-		</View>
+			</View>
+		</TouchableOpacity>
 	);
+};
+
+ListName.propTypes = {
+	id: PropTypes.number.isRequired,
+	name: PropTypes.string.isRequired,
+	color: PropTypes.string.isRequired,
+	onLongPress: PropTypes.func.isRequired,
+	isSelected: PropTypes.bool.isRequired,
 };
 
 export default ListName;
--- Toodler/src/components/ListName/styles.js
+++ Toodler/src/components/ListName/styles.js
@@ -2,20 +2,28 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
 	text: {
-		margin: 10,
+		margin: 13,
 		textAlign: "center",
+		fontWeight: "bold",
+		fontSize: 17,
 	},
 	button: {
 		paddingTop: 10,
 		paddingBottom: 10,
 		marginBottom: 10,
 		marginTop: 10,
-		marginLeft: 20,
-		marginRight: 20,
+		marginLeft: 40,
+		marginRight: 40,
 		paddingLeft: 20,
 		paddingRight: 20,
-		borderWidth: 3,
+		borderWidth: 2,
 		borderColor: "black",
 		borderRadius: 5,
+	},
+	checkmark: {
+		position: "absolute",
+		top: 15,
+		right: 15,
+		fontSize: 16,
 	},
 });
